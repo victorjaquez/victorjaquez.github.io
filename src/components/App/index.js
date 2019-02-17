128dfe6b5cb5ca33d5b67d52be9b488f4fd15e83
victorjaquez <victor@jaquez.me>
2019-02-17 16:53:22 -0700

    homepage styling, buttons with routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "./styles.css";
 import HomePage from "../HomePage";
-import Login from "../Login";
-import Register from "../Register";
-import Dashboard from "../Dashboard";
+// import Login from "../Login";
+// import Register from "../Register";
+// import Dashboard from "../Dashboard";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { CssBaseLine } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const theme = createMuiTheme();
 
 export default function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <CssBaseLine />
+      <CssBaseline />
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/dashboard" component={Dashboard} /> */}
         </Switch>
       </Router>
     </MuiThemeProvider>
