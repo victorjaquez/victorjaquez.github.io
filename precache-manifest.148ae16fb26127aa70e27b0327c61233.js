dc4ee48fef34d8b6deb3293b738b5e657ab10475
victorjaquez <victor@jaquez.me>
2019-02-18 10:35:41 -0700

    Updates

---
+++ precache-manifest.148ae16fb26127aa70e27b0327c61233.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ad836222bfd751f0dfd",
+    "url": "/static/css/main.609a1ae6.chunk.css"
+  },
+  {
+    "revision": "4ad836222bfd751f0dfd",
+    "url": "/static/js/main.4ad83622.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ceee7b5902a4c4f2abaa",
+    "url": "/static/js/2.ceee7b59.chunk.js"
+  },
+  {
+    "revision": "b1577683fc41a9afaff8cb7047cc5369",
+    "url": "/index.html"
+  }
+];
